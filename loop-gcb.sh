d2c9dee8426f12b1241792c77bc905f529e1c2ae
Dave Stanke <davidstanke@gmail.com>
2020-05-11 22:56:24 +0000

    fix loops and other

--- loop-gcb.sh
+++ loop-gcb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-set +eEx o pipefail
+set +eExo pipefail
 
 NUMBUILDS=${1-2} # number of builds to queue per project; default to 2
 PROJECT_LABEL=${2} # label of the projects we're building in
@@ -7,26 +7,28 @@ NUM_PROJECTS=${3} # number of projects
 
 PROJECT_ID=$(gcloud config get-value "project")
 
+echo "running $NUMBUILDS builds in each of $PROJECT_ID's subprojects with label $PROJECT_LABEL"
+
 # initialize a log file
 rm -f buildlog
 touch buildlog
 
-for (( j=0; j<$NUM_PROJECTS; j++)); do
+for ((j=0; j<=$NUM_PROJECTS-1; j++)); do
     THIS_PROJECT="$PROJECT_ID-$PROJECT_LABEL-$j"
     echo "Starting builds in project $THIS_PROJECT..."
     echo "------------------"
-    for (( i = 1; i <= $NUMBUILDS; i++ )); do
-        gcloud builds submit --no-source --substitutions=_BUILD_ITER=${i} --project-id=$THIS_PROJECT >>./buildlog 2>&1 & disown; 
-        echo "Submitted build: ${j}.${i}";
+    for ((i=1; i<=$NUMBUILDS; i++)); do
+        gcloud builds submit --no-source --substitutions=_BUILD_ITER=${i} --project=$THIS_PROJECT >>./buildlog 2>&1 & disown; 
+        echo "Submitted build: ${j}.${i}"
     done
 done    
 
-working=1
-while [ $working -gt 0 ]; do
-  echo "Checking build status..."
-  queued=$(gcloud builds list --limit=300 | grep QUEUED | wc -l)
-  working=$(gcloud builds list --limit=300 | grep WORKING | wc -l)
-  echo "Queued:  $queued"
-  echo "Running: $working"
-  echo
-done
+# working=1
+# while [ $working -gt 0 ]; do
+#   echo "Checking build status..."
+#   queued=$(gcloud builds list --limit=300 | grep QUEUED | wc -l)
+#   working=$(gcloud builds list --limit=300 | grep WORKING | wc -l)
+#   echo "Queued:  $queued"
+#   echo "Running: $working"
+#   echo
+# done
