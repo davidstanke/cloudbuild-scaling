a0d934ad0e706054528cd55b1c1e8eb929f503ed
Don McCasland <donmccasland@google.com>
2020-05-12 13:27:08 -0700

    Send logs to central project bucket.  Fix random id gen to work for OSX.

--- setup.sh
+++ setup.sh
@@ -13,8 +13,12 @@ gcloud services enable cloudbuild.googleapis.com -q
 PROJECTS=${1-1}
 
 # generate a short unique tag to use on all child projects
-export PROJECT_LABEL=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 3 | head -n 1)
+export PROJECT_LABEL=$(LC_CTYPE=C tr -dc 'a-z0-9' < /dev/urandom | fold -w 3 | head -n 1)
 echo "Label for new projects: $PROJECT_LABEL"
+
+echo "Creating central GCS bucket for build logs..."
+echo gsutil mb -p $PROJECT_ID -c standard -l multi-region gs://${PROJECT_ID}-builds
+gsutil mb -p $PROJECT_ID -c standard -l US gs://${PROJECT_ID}-builds
 
 for((i=0;i<$PROJECTS;i++)); do
     export PROJECT_NEW="$PROJECT_ID-p-$PROJECT_LABEL-$i"
@@ -30,6 +34,10 @@ for((i=0;i<$PROJECTS;i++)); do
     fi
     gcloud beta billing projects link $PROJECT_NEW --billing-account $BILLING_ACCOUNT
     gcloud services enable cloudbuild.googleapis.com --project=$PROJECT_NEW
+    PROJECT_NEW_NUM=$(gcloud projects list --filter="${PROJECT_NEW}" --format="value(PROJECT_NUMBER)")
+    gcloud projects add-iam-policy-binding $PROJECT_ID \
+      --member serviceAccount:${PROJECT_NEW_NUM}@cloudbuild.gserviceaccount.com \
+      --role roles/storage.admin
 done
 
 echo "----------------------"
